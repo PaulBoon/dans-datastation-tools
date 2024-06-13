e4438fc169e8175bbc95b4009e5e6186f03dc713
Alias <93505057+aliassheikh@users.noreply.github.com>
2024-06-13 14:12:05 +0200

    DD-1546 Create Cron jobs to create deposit reports for SWORD customers and application managers (#60)
    * DD-1546-Create-Cron-tab-jobs-to-make-deposits-reports
    
    * Update src/datastation/common/send_mail.py
    
    * Update src/datastation/common/send_mail.py
    
    ---------
    
    Co-authored-by: Ali Sheikhi <aliassheikh@users.noreply.github.com>
    Co-authored-by: Jan van Mansum <jan.van.mansum@dans.knaw.nl>

--- src/datastation/common/send_mail.py
+++ src/datastation/common/send_mail.py
@@ -3,14 +3,22 @@ import os
 # https://docs.python.org/3/library/email.html#module-email
 # https://docs.python.org/3/library/email.examples.html
 
+
 class SendMail:
     # def __init__(self):
     #     pass
 
     @staticmethod
-    def send(to_email, subject, message_body, attachment):
-        email_template = " -s '{0}' {1}<<EOF {2} \nEOF"
+    def send(to_email, subject, message_body, attachment, cc_email, bcc_email):
+        recipients = to_email
+
+        if cc_email is not None:
+            recipients = ' -c {0} {1} '.format(cc_email, recipients)
+
+        if bcc_email is not None:
+            recipients = ' -b {0} {1} '.format(bcc_email, recipients)
+
         if attachment is not None:
-            os.system("mail" + " -a " + attachment + email_template.format(subject, to_email, message_body))
+            os.system('echo "{0}" | mail -s "{1}" -a "{2}" {3}'.format(message_body, subject, attachment, recipients))
         else:
-            os.system("mail" + email_template.format(subject, to_email, message_body))
+            os.system('echo "{0}" | mail -s "{1}" {2}'.format(message_body, subject, recipients))
--- src/datastation/deposit_create_report.py
+++ src/datastation/deposit_create_report.py
@@ -19,19 +19,20 @@ class ReportHandler:
             else:
                 self.save_report_to_file(report, output_file)
 
-                email_to = self.__command_line_args.email_to
-                if email_to is not None:
-                    self.send_report_mail(email_to, output_file)
+                if self.__command_line_args.email_to is not None:
+                    self.send_report_mail(output_file)
 
     def save_report_to_file(self, report, output_file):
         with open(output_file, 'w') as output:
             output.write(report)
 
-    def send_report_mail(self, mail_to, attachment):
-        SendMail.send(mail_to,
+    def send_report_mail(self, attachment):
+        SendMail.send(self.__command_line_args.email_to,
                       "Deposits report",
                       "Please, find attached the detailed report of deposits.",
-                      attachment)
+                      attachment,
+                      self.__command_line_args.cc_email_to,
+                      self.__command_line_args.bcc_email_to)
 
 
 def main():
@@ -39,13 +40,15 @@ def main():
     parser = argparse.ArgumentParser(prog='deposit_create_report',
                                      description='Create and send reports based on dd-manage-deposit database')
     parser.add_argument('-o', '--output-file', dest='output_file', default='-',
-                        help='the file to write the output to or - for stdout')
+                        help='the file to write the output and send recipient to a to or - for stdout')
     parser.add_argument('-e', '--enddate', dest='enddate', help='Filter until the record creation of this date')
     parser.add_argument('-s', '--startdate', dest='startdate', help='Filter from the record creation of this date')
     parser.add_argument('-t', '--state', help='The state of the deposit')
     parser.add_argument('-u', '--user', dest='user', help='The depositor name')
     parser.add_argument('-f', '--format', dest='file_format', default='text/csv', help='Output data format')
-    parser.add_argument('--email-to', dest='email_to', help='')
+    parser.add_argument('--email-to', dest='email_to', help='when more than one recipient: comma separated emails')
+    parser.add_argument('--cc-email-to', dest='cc_email_to', help='will be sent only if email-to is defined')
+    parser.add_argument('--bcc-email-to', dest='bcc_email_to', help='will be sent only if email-to is defined')
     args = parser.parse_args()
 
     server_url = config['manage_deposit']['service_baseurl'] + '/report'
